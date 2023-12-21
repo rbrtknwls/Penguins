79c20ce41a71c861a7f9db91b50f64df7e784872
rbrtknwls <r2knowle@uwaterloo.ca>
2023-12-20 20:26:25 -0500

    woo rocks and grass

--- game/global.js
+++ game/global.js
@@ -5,3 +5,7 @@ const ctx = canvas.getContext("2d");
 
 const treeSrc = document.createElement("img");
 treeSrc.src = "source/pixel_art/tree.png"
+const rockSrc = document.createElement("img");
+rockSrc.src = "source/pixel_art/rock.png"
+const grassSrc = document.createElement("img");
+grassSrc.src = "source/pixel_art/grass.png"
--- game/scene/snow.js
+++ game/scene/snow.js
@@ -1,21 +1,23 @@
-function updateObject(obj) {
-	obj.x += 10;
-	obj.y += 10;
-}
+// =================== Object Classes ===================
 
-
-class Tree {
+class backgroundObject {
 	constructor() {
 		if (Math.random() < 0.5) { this.x = canvas.width/2 - 20 }
 		else                     { this.x = canvas.width/2 + 20 }
 
 		this.y = 0;
 
-		this.width = 50;
-		this.ratio = 2;
-
 		this.vel = 1;
 		this.acc = 0.7;
+	}
+
+}
+
+class Tree extends backgroundObject {
+	constructor() {
+		super(backgroundObject);
+		this.width = 50;
+		this.ratio = 2;
 	}
 
 	updatePosition() {
@@ -23,43 +25,116 @@ class Tree {
 		this.y += this.vel;
 		this.width += this.vel;
 
-		if (this.x < canvas.width/2) { this.x -= this.vel*4  }
-		else                         { this.x += this.vel*3    }
+		if (this.x < canvas.width/2) { this.x -= this.vel*3;  }
+		else                         { this.x += this.vel*3;  }
 
-		this.vel += this.acc
+		this.vel += this.acc;
 	}
 
-	drawTree() {
-		ctx.drawImage(treeSrc, this.x, this.y, this.width, this.width*this.ratio)
+	draw() {
+		ctx.drawImage(treeSrc, this.x-this.width/2, this.y, this.width, this.width*this.ratio)
+	}
+}
+
+class Rock extends backgroundObject {
+	constructor() {
+		super(backgroundObject);
+		this.y = 50
+		this.width = 10;
+		this.ratio = 1;
+
+		this.type = Math.floor(Math.random()*3)
 	}
 
+	updatePosition() {
+
+		this.y += this.vel;
+		this.width += this.vel/3;
+
+		if (this.type == 0) { this.x -= this.vel}
+		if (this.type == 2) { this.x += this.vel}
+
+		this.vel += this.acc*1.1;
+	}
+
+	draw() {
+		ctx.drawImage(rockSrc, this.x-this.width/2, this.y, this.width, this.width*this.ratio)
+	}
+}
+
+class Grass extends backgroundObject {
+	constructor() {
+		super(backgroundObject);
+		this.y = 50
+
+		this.width = 10;
+		this.ratio = 1;
+
+		this.type = Math.floor(Math.random()*3)
+	}
+
+	updatePosition() {
+
+		this.y += this.vel;
+		this.width += this.vel/3;
+
+		if (this.type == 0) { this.x -= this.vel;}
+		if (this.type == 2) { this.x += this.vel;}
+
+		this.vel += this.acc*1.1;
+	}
+
+	draw() {
+		ctx.drawImage(grassSrc, this.x-this.width/2, this.y, this.width, this.width*this.ratio)
+	}
 }
 
 class Foliage {
 	constructor() {
-		this.treeList = []
+		this.objectList = []
 	}
 
 	addTree() {
-		this.treeList.push(new Tree())
+		this.objectList.push(new Tree())
 	}
 
-	drawTree() {
-		for (var i = this.treeList.length - 1; i >= 0; i--) {
-		    this.treeList[i].updatePosition();
+	addGrass() {
+		this.objectList.push(new Grass())
+	}
+
+	addRock() {
+		this.objectList.push(new Rock())
+	}
+
+	draw() {
+		for (var i = this.objectList.length - 1; i >= 0; i--) {
+		    this.objectList[i].updatePosition();
 		}
 
-		for (var i = this.treeList.length - 1; i >= 0; i--) {
+		for (var i = this.objectList.length - 1; i >= 0; i--) {
 
-		    if (this.treeList[i].y > canvas.height) {
-		        this.treeList.splice(i, 1);
+		    if (this.objectList[i].y > canvas.height) {
+		        this.objectList.splice(i, 1);
 		    } else {
-		    	this.treeList[i].drawTree();
+		    	this.objectList[i].draw();
 		    }
 		}
 	}
 
 }
+// =================== Helpers ===================
+
+function drawGround() {
+
+	ctx.fillStyle = "#fffafa";
+	ctx.beginPath();
+    ctx.moveTo(canvas.width/2, 50);
+    ctx.lineTo(-200, canvas.height);
+    ctx.lineTo(canvas.width+200, canvas.height);
+    ctx.fill();
+}
+
+// =================== Runner ===================
 
 
 Scenes.snow = function(currentIteration) {
@@ -70,10 +145,16 @@ Scenes.snow = function(currentIteration) {
 
 		var intervalId = setInterval(function() {
 			ctx.clearRect(0, 0, canvas.width, canvas.height)
-			if (Math.random() * 2 < 1) { background.addTree(); }
+			if (Math.random() < 0.5) { background.addTree(); }
+			else {
+				if (Math.random() < 0.1) {
+					if (Math.random() < 0.5) { background.addGrass(); }
+					else                     { background.addRock(); }
+				}
+			}
 
-
-			background.drawTree();
+			drawGround();
+			background.draw();
 
 		}, 50);
   	});
